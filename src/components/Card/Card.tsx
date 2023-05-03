e4e2b33492790fb447422e504e04428e6dc514ab
Bumble <bumble.sakh@gmail.com>
2023-05-03 23:22:06 +1100

    feat: card layout

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,30 +1,20 @@
 import styles from './Card.module.scss';
+import pattern from '../../assets/images/bg-pattern-desktop.svg';
+import box from '../../assets/images/illustration-box-desktop.svg';
+import woman from '../../assets/images/illustration-woman-online-desktop.svg';
 
 const Card = () => {
   return (
     <div className={styles.card}>
-      <div className={styles.top}></div>
+      <div className={styles.img}>
+        <img className={styles.pattern} src={pattern} alt='pattern' />
+        <img className={styles.box} src={box} alt='box' />
+        <img className={styles.woman} src={woman} alt='woman' />
+      </div>
       <div className={styles.content}>
-        <div className={styles.header}>
-          <h1 className={styles.name}>Victor Crest</h1>
-          <span className={styles.age}>26</span>
-        </div>
-        <span className={styles.city}>London</span>
-        <div className={styles.line}></div>
-        <div className={styles.info}>
-          <div className={styles.block}>
-            <p className={styles.title}>80K</p>
-            <p className={styles.subtitle}>Followers</p>
-          </div>
-          <div className={styles.block}>
-            <p className={styles.title}>803K</p>
-            <p className={styles.subtitle}>Likes</p>
-          </div>
-          <div className={styles.block}>
-            <p className={styles.title}>1.4K</p>
-            <p className={styles.subtitle}>Photos</p>
-          </div>
-        </div>
+        <h1 className={styles.name}>FAQ</h1>
+
+        {/* Question component */}
       </div>
     </div>
   );
