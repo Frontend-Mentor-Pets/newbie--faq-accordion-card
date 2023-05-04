5d0ae12a65d172cdca84023a845e7c2882760f40
Bumble <bumble.sakh@gmail.com>
2023-05-04 18:25:43 +1100

    feat: add styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,23 @@
 import styles from './Card.module.scss';
-import pattern from '../../assets/images/bg-pattern-desktop.svg';
 import box from '../../assets/images/illustration-box-desktop.svg';
 import woman from '../../assets/images/illustration-woman-online-desktop.svg';
+import questions from '../../assets/data';
+import Question from '../Question';
 
 const Card = () => {
   return (
     <div className={styles.card}>
       <div className={styles.img}>
-        <img className={styles.pattern} src={pattern} alt='pattern' />
-        <img className={styles.box} src={box} alt='box' />
         <img className={styles.woman} src={woman} alt='woman' />
+        <img className={styles.box} src={box} alt='box' />
       </div>
       <div className={styles.content}>
-        <h1 className={styles.name}>FAQ</h1>
-
-        {/* Question component */}
+        <h1 className={styles.header}>FAQ</h1>
+        <ul className={styles.questions}>
+          {questions.map(({ question, answer }, id) => (
+            <Question key={id} id={String(id)} question={question} answer={answer} />
+          ))}
+        </ul>
       </div>
     </div>
   );
